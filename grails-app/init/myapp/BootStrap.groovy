efb399cbb13b99b0d2f7a57410b7acfe317a218c
zyro <zyro@zyro.net>
2017-06-04 21:30:31 +0200

    sample app for grails/grails-core#10658

--- grails-app/init/myapp/BootStrap.groovy
+++ grails-app/init/myapp/BootStrap.groovy
@@ -1,8 +1,16 @@
 package myapp
 
+import grails.core.GrailsApplication
+import org.grails.spring.aop.autoproxy.GroovyAwareAspectJAwareAdvisorAutoProxyCreator
+
 class BootStrap {
 
+    GrailsApplication grailsApplication
+
     def init = { servletContext ->
+        def autoProxyCreator = grailsApplication.mainContext.getBean("org.springframework.aop.config.internalAutoProxyCreator")
+        assert autoProxyCreator in GroovyAwareAspectJAwareAdvisorAutoProxyCreator
+        assert autoProxyCreator.proxyTargetClass
     }
     def destroy = {
     }
